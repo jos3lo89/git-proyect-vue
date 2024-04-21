3f3f6b0df6c91a8834ab118f47cae92cbeb16739
joselo999 <strange33g@gmail.com>
2024-04-21 14:30:55 -0500

    add h3

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@ const numero = ref(0);
 <template>
   <h1>hola mundo wadafa</h1>
   <h2>hola soy el contribuidor</h2>
+  <h3>hola soy el master y agregue esta linia</h3>
 
   <button @click="numero++">{{ numero }}</button>
 </template>
